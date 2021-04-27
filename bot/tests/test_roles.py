7ce307b156a5dcd09993832614cf949d010a33b9
Raymond van Egmond <raymond@goingcloud.eu>
2021-04-27 19:45:07 +0200

    update9

---
+++ bot/tests/test_roles.py
@@ -0,0 +1,28 @@
+import unittest
+import pytest
+from mock import MagicMock
+from modules.roles import Roles
+
+
+@pytest.mark.asyncio
+async def test_in_role_ok():
+    roles = Roles()
+    req_role = "testrole"
+    role = MagicMock()
+    role.name = 'testrole'
+    ctx = MagicMock()
+    ctx.author.roles = [role]
+    res = await roles.in_role(ctx, req_role)
+    assert res is True
+
+
+@pytest.mark.asyncio
+async def test_in_role_nok():
+    roles = Roles()
+    req_role = "other_role"
+    role = MagicMock()
+    role.name = 'testrole'
+    ctx = MagicMock()
+    ctx.author.roles = [role]
+    res = await roles.in_role(ctx, req_role)
+    assert res is False
