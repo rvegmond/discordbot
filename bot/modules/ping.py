22bb38cc8c53d26ee4868ebf019881a5623cd568
Raymond van Egmond <raymond@goingcloud.eu>
2021-05-05 19:55:25 +0200

    added test ping

--- bot/modules/ping.py
+++ bot/modules/ping.py
@@ -5,16 +5,16 @@ from .robin import Robin
 
 
 class Ping(Robin):
-    # @commands.command(name='ping', hidden=False)
-    # @commands.max_concurrency(1, wait=True)
-    # @commands.guild_only()
+
+    async def _ping(self, ctx):
+        await self._feedback(ctx, msg="pong")
+
     @commands.command(
-        
         name="ping",
         help=("Commando om te zien of de bot nog leeft."
-        "Als de bot een redelijk blije bot is krijg je en pong terug."
-        "Een blije bot is geen garantie op succes en gezondheid...."),        
+              "Als de bot een redelijk blije bot is krijg je en pong terug."
+              "Een blije bot is geen garantie op succes en gezondheid...."),
         brief="Commando om te zien of de bot nog leeft.",
-        )
+    )
     async def ping(self, ctx):
-        await ctx.send(f"pong")
+        await self._ping(ctx)
--- bot/tests/test_whitestar.py
+++ bot/tests/test_whitestar.py
@@ -1,24 +1,24 @@
-import unittest
-import pytest
-from mock import MagicMock
-from modules.whitestar import WhiteStar
+# import unittest
+# import pytest
+# from mock import MagicMock
+# from modules.whitestar import WhiteStar
 
-bot = MagicMock()
+# bot = MagicMock()
+
+# # @pytest.mark.asyncio
+# # async def test_update_comeback_channel():
+# #     whitestar = Whitestar()
+
+# #     assert res is True
 
 # @pytest.mark.asyncio
-# async def test_update_comeback_channel():
-#     whitestar = Whitestar()
-
+# async def test_update_usermap():
+#     """
+#     Need to write the test, using mock db
+#     """
+#     whitestar = WhiteStar(bot)
+#     ctx = MagicMock()
+#     role = MagicMock()
+#     role.name = 'testrole'
+#     res = True
 #     assert res is True
-
-@pytest.mark.asyncio
-async def test_update_usermap():
-    """
-    Need to write the test, using mock db
-    """
-    whitestar = WhiteStar(bot)
-    ctx = MagicMock()
-    role = MagicMock()
-    role.name = 'testrole'
-    res = True
-    assert res is True
