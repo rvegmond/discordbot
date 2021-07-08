377255f877d8721c20bc186170ba4a1333b3f9c2
Raymond van Egmond <raymond@goingcloud.eu>
2021-07-08 15:13:15 +0200

    adding on_message

--- bot/bot.py
+++ bot/bot.py
@@ -9,20 +9,23 @@ from discord.ext import commands
 from loguru import logger
 from modules import whitestar, ping, roles
 
-DB_FILE = '../data/hades.db'
+DB_FILE = "../data/hades.db"
 
 ___VERSION___ = "[v1.4.0]"
 
 config = {
     "handlers": [
-        {"sink": sys.stdout,
-         "format": ___VERSION___ + " [{time:YYYY-MM-DD at HH:mm:ss}] [{level}]: {message}"}
+        {
+            "sink": sys.stdout,
+            "format": ___VERSION___
+            + " [{time:YYYY-MM-DD at HH:mm:ss}] [{level}]: {message}",
+        }
     ],
 }
 
 
 def create_connection():
-    """ create a database connection to the SQLite database
+    """create a database connection to the SQLite database
         specified by db_file
     :param db_file: database file
     :return: Connection object or None
@@ -39,13 +42,20 @@ def create_connection():
 
 
 def new_bot(command_prefix: str, description: str) -> discord.ext.commands.bot:
-    """ Create a new discordbot
-    """
+    """Create a new discordbot"""
 
     intents = discord.Intents.default()
     intents.members = True
-    bot = commands.Bot(command_prefix=command_prefix, description=description, intents=intents)
+    bot = commands.Bot(
+        command_prefix=command_prefix, description=description, intents=intents
+    )
     conn = create_connection()
+
+    @bot.event
+    async def on_message(message):
+        logger.info(f"message {message}")
+
+        await bot.process_commands(message)
 
     @bot.event
     async def on_ready():
@@ -54,13 +64,15 @@ def new_bot(command_prefix: str, description: str) -> discord.ext.commands.bot:
         bot.add_cog(ping.Ping(bot))
         bot.add_cog(whitestar.WhiteStar(bot, conn))
         bot.add_cog(roles.Roles(bot, conn))
-        # bot.add_cog(scheduler.Scheduler(bot, conn))
+
     return bot
 
 
 if __name__ == "__main__":
     logger.configure(**config)
     logger.info("Now loading...")
-    b = new_bot(command_prefix=os.getenv("COMMAND_PREFIX", "!"),
-                description=os.getenv("BOT_DESCRIPTION", f"Robin, version {___VERSION___ }"))
+    b = new_bot(
+        command_prefix=os.getenv("COMMAND_PREFIX", "!"),
+        description=os.getenv("BOT_DESCRIPTION", f"Robin, version {___VERSION___ }"),
+    )
     b.run(os.getenv("DISCORD_TOKEN"))
