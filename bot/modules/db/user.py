25b56373900275a6819871a5426f289a1d5ca8ae
Raymond van Egmond <raymond@goingcloud.eu>
2021-07-23 19:53:30 +0200

    update

--- bot/modules/db/user.py
+++ bot/modules/db/user.py
@@ -12,6 +12,7 @@ class User(Base):
     UserId = Column(Integer, primary_key=True)
     DiscordId = Column(String)
     DiscordAlias = Column(String)
+    DiscordNick = Column(String)
     GsheetAlias = Column(String)
     LastActive = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     LastChannel = Column(String)
@@ -21,6 +22,7 @@ class User(Base):
             f"<User(UserId={self.UserId},"
             f"DiscordId={self.DiscordId},"
             f"DiscordAlias={self.DiscordAlias},"
+            f"DiscordNick={self.DiscordNick},"
             f"GsheetAlias={self.GsheetAlias},"
             f"LastActive={self.LastActive},"
             f"LastChannel={self.LastChannel})>"
