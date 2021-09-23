a3027ca6a4b5663916dbd282dbb6a618fac2e2b8
Raymond van Egmond <raymond@goingcloud.eu>
2021-09-23 17:56:05 +0200

    added files

---
+++ bot/modules/members/__init__.py
@@ -0,0 +1,16 @@
+"""
+All related to whitestar functionality
+"""
+import locale
+import os
+import datetime
+from datetime import timedelta, datetime
+from .tassadar import *
+from .shamrock import *
+
+# try:
+#     locale.setlocale(locale.LC_ALL, "nl_NL.utf8")  # required running on linux
+#     logger.info("running on linux")
+# except locale.Error:
+#     locale.setlocale(locale.LC_ALL, "nl_NL.UTF-8")  # required when running on MAC
+#     logger.info("running on mac")
---
+++ bot/modules/members/tassadar.py
@@ -0,0 +1,23 @@
+"""
+The contents of this file is to reflect happyness of robin.
+"""
+from discord.ext import commands
+from ..robin import Robin
+from ..utils import feedback
+
+
+class Tassadar(Robin):
+    """
+    Are you happy Robin?
+    """
+
+    @commands.command(
+        name="tassadar",
+        help="Hoe is het met tassadar?",
+        brief="Hoe is het met tassadar?",
+    )
+    async def command(self, ctx):
+        """
+        How are you Tass?
+        """
+        await feedback(ctx, msg="(https://www.youtube.com/watch?v=rBDwUXi1Sbw)")
