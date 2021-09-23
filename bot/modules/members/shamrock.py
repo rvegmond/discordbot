e60af1bf04b01c5d3ca36278e30263468633e4db
Raymond van Egmond <raymond@goingcloud.eu>
2021-09-23 17:56:05 +0200

    added files

---
+++ bot/modules/members/shamrock.py
@@ -0,0 +1,26 @@
+"""
+The contents of this file is to reflect happyness of robin.
+"""
+from discord.ext import commands
+from ..robin import Robin
+from ..utils import feedback
+
+
+class Shamrock(Robin):
+    """
+    Are you happy Robin?
+    """
+
+    @commands.command(
+        name="shamrock",
+        help="Hoe is het met shamrock?",
+        brief="Hoe is het met tassadar?",
+    )
+    async def command(self, ctx):
+        """
+        How are you Sham?
+        """
+        await feedback(
+            ctx,
+            msg="Voelt zich niet zo lekker,\n had gister dat laatste biertje niet moeten nemen...",
+        )
